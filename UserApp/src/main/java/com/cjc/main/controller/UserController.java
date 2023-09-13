e7473a996b54fbb15c27209cdde8509f470eaadd
Himanshukhode99 <himanshukhode001@.gmail.com>
2023-09-13 15:50:21 +0530

    deletedata api complete

--- UserApp/src/main/java/com/cjc/main/controller/UserController.java
+++ UserApp/src/main/java/com/cjc/main/controller/UserController.java
@@ -4,10 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import org.springframework.web.bind.annotation.GetMapping;
+
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +23,7 @@ import com.cjc.main.service.UserService;
 public class UserController {
 	@Autowired
 	UserService us;
+
 
 	
 	@PostMapping("/save")
@@ -33,4 +39,19 @@ public class UserController {
 		Iterable<User> user=us.getAllData();
 		return new ResponseEntity<Iterable<User>>(user, HttpStatus.OK);
 	}
+
+	
+	
+	@DeleteMapping("/deletedata/{uId}")
+	public ResponseEntity<User>deletedata(@PathVariable int uId){
+	
+		us.deletedata(uId);
+		
+		
+		return new ResponseEntity<User>(HttpStatus.OK) ;
+		
+	}
+	
+	
+
 }
