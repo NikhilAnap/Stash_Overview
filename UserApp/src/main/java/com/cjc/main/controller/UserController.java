724b5f2e5b2e4bfbe9d298f6bc9ed0d5e660205f
NikhilAnap <nikanap19@gmail.com>
2023-09-13 15:43:52 +0530

    Revert "Get Single API Imp"
    This reverts commit 1a554800444d8b6f4d16ee1755ddc78de42ba50d.

--- UserApp/src/main/java/com/cjc/main/controller/UserController.java
+++ UserApp/src/main/java/com/cjc/main/controller/UserController.java
@@ -1,28 +1,8 @@
 package com.cjc.main.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.cjc.main.model.User;
-import com.cjc.main.service.UserService;
 
 @RestController
 public class UserController {
-
-	@Autowired
-	UserService us;
-
-	@GetMapping("/getSingle/{id}")
-	public ResponseEntity<User> getSingle(@PathVariable int id) {
-
-		User user = us.getSingle(id);
-
-		return new ResponseEntity<User>(user, HttpStatus.OK);
-
-	}
 
 }
--- UserApp/src/main/java/com/cjc/main/service/UserService.java
+++ UserApp/src/main/java/com/cjc/main/service/UserService.java
@@ -1,9 +1,5 @@
 package com.cjc.main.service;
 
-import com.cjc.main.model.User;
-
 public interface UserService {
-
-	public User getSingle(int id);
 
 }
