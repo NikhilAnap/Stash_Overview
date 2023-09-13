1a554800444d8b6f4d16ee1755ddc78de42ba50d
NikhilAnap <nikanap19@gmail.com>
2023-09-13 15:38:08 +0530

    Get Single API Imp

--- UserApp/src/main/java/com/cjc/main/service/UserService.java
+++ UserApp/src/main/java/com/cjc/main/service/UserService.java
@@ -1,5 +1,9 @@
 package com.cjc.main.service;
 
+import com.cjc.main.model.User;
+
 public interface UserService {
+
+	public User getSingle(int id);
 
 }
--- UserApp/src/main/java/com/cjc/main/serviceimpl/UserServiceImpl.java
+++ UserApp/src/main/java/com/cjc/main/serviceimpl/UserServiceImpl.java
@@ -1,10 +1,32 @@
 package com.cjc.main.serviceimpl;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.cjc.main.model.User;
+import com.cjc.main.repository.UserRepository;
 import com.cjc.main.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+	@Autowired
+	UserRepository ur;
+
+	@Override
+	public User getSingle(int id) {
+
+		Optional<User> user = ur.findById(id);
+
+		if (user.isPresent()) {
+
+			return user.get();
+
+		}
+
+		return null;
+	}
 
 }
